eb631689c769667be5ac5e7aa7ab5a6e903089e8
moacirviana <moacir.viana@gmail.com>
2022-06-19 12:34:50 -0400

    Tela sobre e outros ajustes do router

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,6 +2,7 @@ import Menu from 'components/Menu';
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -23,6 +24,14 @@ export default function Inicio() {
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt='casa do alunori' />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3127 <br/><br/> Vila Mariana, SP
+        </div>
+
       </div>
     </section>
   );
