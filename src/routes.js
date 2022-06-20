a81a1b60c3b9985cd0847f86986ffd2107c0fd46
015697172275 <moacir.viana@tre-am.jus.br>
2022-06-20 07:34:03 -0400

    botao voltar com useNavigate

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<Inicio />} />
-          <Route path='cardapio' element={<Cardapio />} />
-          <Route path='sobre' element={<Sobre />} />
-        </Route>
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main> 
   );
 }
